290da9e808fba971bdb7ff5da8e5268172c4920b
Ines Sacristan <inessacristan13@gmail.com>
2026-09-10 13:28:19 +0200

    Configura CORS para frontend

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const wishlistRoutes = require('./routes/wishlist');
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5175");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
